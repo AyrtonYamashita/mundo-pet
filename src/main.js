5a1044fa28b94c87665e5562cfe1a8165e404b8e
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2024-09-04 10:09:24 -0400

    Instalando o HTMLWebPackPlugin, CopyWebpackPlugin, style-loader e css-loader

---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+/* IMPORTANDO CSS */
+import "./styles/form.css"
+import "./styles/global.css"
+import "./styles/main.css"
+import "./styles/sections.css"
+
+/* IMPORTANDO JS */
+import "./modules/form/show-form.js"
