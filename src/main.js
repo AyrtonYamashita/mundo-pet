044f0264e4aabf6ae05bc801cf7a20d3d309d253
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2024-09-05 16:11:14 -0400

    Criando expressão regular para input de telefone

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import "./modules/form/show-form.js"
 import "./modules/form/submit.js"
 import "./modules/form/show-hours.js"
 import "./modules/form/page-load.js"
+import "./modules/form/format-phone.js"
 
 /* IMPORTANDO LIBS */
 import "./libs/dayjs.js"
---
+++ src/modules/form/format-phone.js
@@ -0,0 +1,20 @@
+const phone = document.querySelector("#phone");
+
+phone.addEventListener("input", () => {
+  // Remove todos os caracteres que não são números
+  let value = phone.value.replace(/\D/g, "");
+
+  // Aplica a formatação: (XX) XXXXX-XXXX
+  if (value.length > 10) {
+    value = value.replace(/^(\d{2})(\d{5})(\d{4}).*/, "($1) $2-$3");
+  } else if (value.length > 6) {
+    value = value.replace(/^(\d{2})(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+  } else if (value.length > 2) {
+    value = value.replace(/^(\d{2})(\d{0,5})/, "($1) $2");
+  } else {
+    value = value.replace(/^(\d*)/, "($1");
+  }
+
+  // Atualiza o valor do input
+  phone.value = value;
+});
